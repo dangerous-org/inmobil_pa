a2aee898a9e5e4e9f0d6b8d252430ee17417b7c1
andresdavidev <andresdavidpachecocuadro@gmail.com>
2023-10-23 16:53:06 -0500

    algunas correciones del código anterior

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-import { Server } from './models/server.js'
 import 'dotenv/config.js'
+import { Server } from './models/server.js'
+
 const server = new Server();
 
 server.listen();
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,10 +1,17 @@
-import { response } from "express";
-import UserModel from "../models/user.js";
+import { request, response } from "express";
+import UserModel from "../models/user.model.js";
 
 export const signUp = async (req = request, res = response) => {
     await UserModel.signup(req, res);
   };// controlador para crear un nuevo usuario
   
 export const signIn = async(req = request, res = response) => {
-    await UserModel.signIn(req,res).catch(err => console.log(err));
+    await UserModel.signIn(req,res);
 }//Controlador para iniciar sesion
+
+export const logOut = (req = request, res = response)=>{
+  res.cookie("authToken", "",{
+    expires: new Date(0),
+  });
+  return res.status(200).json({message: "log out succesfuly"});
+}
