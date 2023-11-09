abd4522b92f469128ace97e2fda3689e4c0043c9
andresdavidev <andresdavidpachecocuadro@gmail.com>
2023-11-08 19:19:49 -0500

    algunas cositas modificadas

--- schemas/auth.schema.js
+++ schemas/auth.schema.js
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const userSchemaSignUp = z.object({
   user: z.string().min(6, {
     message: "Invalid User min 6 characters",
-  }),
+  }).max(15),
   password: z.string().min(8, {
     message: "Invalid password min 8 characters",
   }),
